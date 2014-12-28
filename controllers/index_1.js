e77c716823fbf95e73823ccd0e4dfdaedd3d1d38
Georgi Prodanov <georgiwe@gmail.com>
2014-12-28 21:24:42 +0200

    Fix user register and login .jade files. Edit footer, application and page title. Add .gitignore for "uploads" folder.

--- controllers/index_1.js
+++
@@ -1,9 +0,0 @@
-var usersController = require('./users-controller');
-var userUploadsController = require('./user-uploads-controller');
-var homepageController = require('./home-controller');
-
-module.exports = {
-	users: usersController,
-	uploads: userUploadsController,
-	home: homepageController
-};
