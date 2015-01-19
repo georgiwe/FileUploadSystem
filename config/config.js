7717729589d408392b3448982392c354a18dadcf
Georgi Prodanov <georgiwe@gmail.com>
2015-01-19 15:49:59 +0200

    Fix connection string username. Center text on info messages.

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     development: {
         rootPath: rootPath,
         // dbConn: 'mongodb://localhost:27017/fileuploadsystem',
-        dbConn: 'mongodb://fileuploadsys:qweqwe@ds033831.mongolab.com:33831/fileuploadsystem',
+        dbConn: 'mongodb://fileupsys:qweqwe@ds033831.mongolab.com:33831/fileuploadsystem',
         uploadsPath: path.normalize(rootPath + '/uploads/'),
         port: process.env.PORT || 1234
     }
